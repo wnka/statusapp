c35533db102ca6abdb64e1c95829adf508a7d62c
Phil Piwonka (flipper) <philpiwonka@gmail.com>
2010-12-24 17:29:47 -0800

    Cleanup

--- lists/today.js
+++ lists/today.js
@@ -8,6 +8,11 @@ function(head, req) {
     var indexPath = path.list('today','today_status',{descending:false});
 
     var path_parts = req.path;
+
+    var padDigit = function(val) {
+        return (val < 10 ? "0" : "") + val;
+    }
+
     // The provides function serves the format the client requests.
     // The first matching format is sent, so reordering functions changes 
     // thier priority. In this case HTML is the preferred format, so it comes first.
@@ -20,11 +25,7 @@ function(head, req) {
             posts : List.withRows(function(row) {
                 var post = row.value;
                 var d = new Date(Date.parse(post.created_at));
-                var currentHours = d.getHours();
-                var currentMinutes = d.getMinutes();
-                currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
-                currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-                var timeString = currentHours + ":" + currentMinutes;
+                var timeString = padDigit(d.getHours()) + ":" + padDigit(d.getMinutes());
                 return {
                     time : timeString,
                     text : post.text
