5825efcb71436d05ff42f19c7fc7b17bc116d9ec
Phil Piwonka (flipper) <philpiwonka@gmail.com>
2010-12-24 17:11:40 -0800

    Added stuff from sofa to get view templates working

--- lists/today.js
+++ lists/today.js
@@ -1,19 +1,39 @@
 function(head, req) {
-    var row; 
-    start({
-        "headers": {
-            "Content-Type": "text/html"
+    var ddoc = this;
+    var Mustache = require("lib/mustache");
+    var List = require("vendor/couchapp/lib/list");
+    var path = require("vendor/couchapp/lib/path").init(req);
+    var Atom = require("vendor/couchapp/lib/atom");
+
+    var indexPath = path.list('today','today_status',{descending:false});
+
+    var path_parts = req.path;
+    // The provides function serves the format the client requests.
+    // The first matching format is sent, so reordering functions changes 
+    // thier priority. In this case HTML is the preferred format, so it comes first.
+    provides("html", function() {
+        var today = new Date();
+        // render the html head using a template
+        var stash = {
+            title : today.toDateString(),
+            scripts : {},
+            db : req.path[0],
+            design : req.path[2],
+            assets : path.asset(),
+            posts : List.withRows(function(row) {
+                var post = row.value;
+                var d = new Date(Date.parse(post.created_at));
+                var currentHours = d.getHours();
+                var currentMinutes = d.getMinutes();
+                currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
+                currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+                var timeString = currentHours + ":" + currentMinutes;
+                return {
+                    time : timeString,
+                    text : post.text
+                }
+            })
         }
+        return Mustache.to_html(ddoc.templates.index, stash, ddoc.templates.partials, List.send);
     });
-    send("<ul>");
-    while (row = getRow()) {
-        var d = new Date(Date.parse(row.value.created_at));
-        var currentHours = d.getHours();
-        var currentMinutes = d.getMinutes();
-        currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
-        currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-        var timeString = currentHours + ":" + currentMinutes;
-        send("<li>" + timeString + " - " + row.value.text + "</li>");
-    }
-    send("</ul>");
-}
+};
