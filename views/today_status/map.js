2d3e2f7aad8048f6eec829ce337dc1ffcf844d32
Phil Piwonka (flipper) <philpiwonka@gmail.com>
2010-12-25 00:28:09 -0800

    Fixed bug with view

--- views/today_status/map.js
+++ views/today_status/map.js
@@ -2,7 +2,7 @@ function(doc) {
     if (doc.type == 'status') {
         var date = Date.parse(doc.created_at); 
         var now = new Date(); 
-        var today = new Date(now.getFullYear(), now.getMonth(), now.getDay()); 
+        var today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); 
         if (date >= today.getTime()) 
             emit(date, {created_at: doc.created_at, text: doc.text});
     }
