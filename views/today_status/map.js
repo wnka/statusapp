279360669655893aaabbc128765903acbadb72bd
Phil Piwonka (flipper) <philpiwonka@gmail.com>
2011-01-10 20:43:01 -0800

    view fixes and removal of scripts in html

--- views/today_status/map.js
+++ views/today_status/map.js
@@ -1,12 +1,14 @@
 function(doc) {
     if (doc.type == 'status') {
         var date = Date.parse(doc.created_at); 
-        var now = new Date(); 
+        var now = new Date();
+        var diff = 8*60*60*1000; 
         var today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); 
+        today = new Date(today.getTime() - diff);
 
         if (date >= today.getTime()) {
             emit(date, doc.text);
-	    }
+        }
     }
 }
 
