e88b4e4dea1cf255cee775ddf11ba5be9e5e1043
Phil Piwonka (flipper) <philpiwonka@gmail.com>
2011-01-11 23:04:04 -0800

    view is now last 24 hours

--- views/today_status/map.js
+++ views/today_status/map.js
@@ -2,9 +2,8 @@ function(doc) {
     if (doc.type == 'status') {
         var date = Date.parse(doc.created_at); 
         var now = new Date();
-        var diff = 8*60*60*1000; 
-        var today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); 
-        today = new Date(today.getTime() - diff);
+        var diff = 24*60*60*1000; 
+        today = new Date(now.getTime() - diff);
 
         if (date >= today.getTime()) {
             emit(date, doc.text);
